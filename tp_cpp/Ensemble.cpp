094e252843d25eea096a0aa0bd5cdafc2b9e691e
lucas <lucaszim16@gmail.com>
2019-10-23 17:11:17 +0200

    de

--- tp_cpp/Ensemble.cpp
+++ tp_cpp/Ensemble.cpp
@@ -172,7 +172,7 @@ unsigned int Ensemble::Retirer(const Ensemble & unEnsemble)
   int cmt = 0;
   for (size_t i = 0; i < unEnsemble.tailleAct; i++) {
     cout << unEnsemble.contenu[i] << " " << "indice : " << i << " ";
-    if(this->Retirer(unEnsemble.contenu[i])){
+    if(Retirer(unEnsemble.contenu[i])){
 
       cmt++;
     }
